5dbd5aafd01cb657725509c55a23474e1a9f0ba8
tricialeach <tleach@gmail.com>
2017-07-06 16:46:09 -0700

    Adding a few more setup steps from tutorial

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:getitdone@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
 
 tasks = []
 
@@ -14,4 +27,5 @@ def todos():
 
     return render_template('todos.html', title="Get It Done!", tasks=tasks)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
