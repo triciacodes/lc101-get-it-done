fa7338e53a3167b45b910c10e118cc6b5435c2c4
tricialeach <tleach@gmail.com>
2017-07-06 17:42:14 -0700

    Completed lesson 2.9

--- main.py
+++ main.py
@@ -11,21 +11,36 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean, default=False)
 
     def __init__(self, name):
         self.name = name
+        self.completed = False
 
-
-tasks = []
 
 @app.route('/', methods=['POST', 'GET'])
-def todos():
+def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    return render_template('todos.html', title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html', title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
